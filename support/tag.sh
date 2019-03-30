ba53c0259140a8726d2c50031aabfc1575ccad09
tparker <tparker@usgs.gov>
2019-03-30 13:43:18 -0800

    tweak tagging script

--- support/tag.sh
+++ support/tag.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+VERSION=`python -c "import rsCollectors; print(tomputils.__version__)"`
+echo $VERSION
 git add rsCollectors/__init__.py
 git commit -m 'version bump'
 git push \
-&& git tag `cat VERSION` \
+&& git tag $VERSION \
 && git push --tags
