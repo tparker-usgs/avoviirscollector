ebdf5f751c5eaf3c6a227ede7a6cf2a6e2a79bcf
Tom Parker <tparker@usgs.gov>
2019-09-10 14:24:49 -0800

    set env var defaults

--- avoviirscollector/__init__.py
+++ avoviirscollector/__init__.py
@@ -19,8 +19,8 @@ from avoviirscollector.version import __version__
 import tomputils.util as tutil
 
 logger = tutil.setup_logging("mirror_gina errors")
-BASE_DIR = tutil.get_env_var("VIIRS_BASE_DIR")
-SATELLITE = tutil.get_env_var("VIIRS_SATELLITE")
-CHANNELS = tutil.get_env_var("VIIRS_CHANNELS").split("|")
+BASE_DIR = tutil.get_env_var("VIIRS_BASE_DIR", "unset")
+SATELLITE = tutil.get_env_var("VIIRS_SATELLITE", "unset")
+CHANNELS = tutil.get_env_var("VIIRS_CHANNELS", "unset").split("|")
 
 __all__ = ["__version__"]
